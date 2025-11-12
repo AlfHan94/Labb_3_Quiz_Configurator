134869207e370243ab0a3cec1cdb3f76c8bda5e5
AlfHan94 <alfredhandin@gmail.com>
2025-11-12 16:49:41 +0100

    +++

--- Labb_3_Quiz_Configurator/App.xaml.cs
+++ Labb_3_Quiz_Configurator/App.xaml.cs
@@ -1,14 +1,43 @@
-﻿using System.Configuration;
-using System.Data;
+﻿using Labb_3_Quiz_Configurator.ViewModels;
 using System.Windows;
+using System.Windows.Input;
 
-namespace Labb_3_Quiz_Configurator
+namespace Labb_3_Quiz_Configurator;
+
+public partial class App : Application
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
-    public partial class App : Application
+    protected override void OnExit(ExitEventArgs e)
     {
-    }
+        var window = Application.Current?.MainWindow;
+        if (window?.DataContext is MainWindowViewModel vm)
+        {
+            System.Diagnostics.Debug.WriteLine(">>> OnExit triggered");
 
+            try
+            {
+                var focused = Keyboard.FocusedElement as UIElement;
+                if (focused != null)
+                {
+                    focused.MoveFocus(new TraversalRequest(FocusNavigationDirection.Next));
+                }
+                else
+                {
+                    window.Focus();
+                    var scope = FocusManager.GetFocusScope(window);
+                    FocusManager.SetFocusedElement(scope, window);
+                }
+
+                vm.LoadTask?.GetAwaiter().GetResult();
+                vm.SavePacksAsync().GetAwaiter().GetResult();
+
+                System.Diagnostics.Debug.WriteLine(">>> Save completed");
+            }
+            catch (System.Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine(">>> Save failed: " + ex);
+            }
+        }
+
+        base.OnExit(e);
+    }
 }
